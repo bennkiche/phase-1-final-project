cd4aee44a08b800b5300ed0e3a24e8f46cf53f0d
Benn <bennkiche7@gmail.com>
2024-10-18 13:27:02 +0300

    deleting server.js file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const corsAnywhere = require('cors-anywhere');
+// const express = require('express');
+// const corsAnywhere = require('cors-anywhere');
 
-const app = express();
-const port = 8080;
+// const app = express();
+// const port = 8080;
 
-corsAnywhere.createServer().listen(port, () => {
-    console.log(`CORS Anywhere running on http://localhost:${port}`);
-});
+// corsAnywhere.createServer().listen(port, () => {
+//     console.log(`CORS Anywhere running on http://localhost:${port}`);
+// });
